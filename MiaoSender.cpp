a7a23ce33db486a1c2b7a8b83ff1c0df7ef54644
AriaPhantom <32724547+AriaPhantom@users.noreply.github.com>
2026-01-28 09:22:54 -0800

    Use shared SPUtils/MiaoSender from GlacialTool_Common

--- MiaoSender.cpp
+++ MiaoSender.cpp
@@ -1,23 +1,2 @@
 #include "stdafx.h"
-#include "MiaoSender.h"
-#include <cpr/cpr.h>
-
-#define SOCKS5_PROXY "socks5h://localhost:1080" 
-
-int sendMiaoCode(std::string code) {
-
-    std::stringstream geturl;
-    geturl << "http://miaotixing.com/trigger?id=";
-    geturl << code;
-
-    std::string result;
-
-    //cpr::Response r = cpr::Get(cpr::Url{ geturl.str().c_str() }, cpr::Proxies{ {"http", SOCKS5_PROXY} }, cpr::VerifySsl(false));
-    cpr::Response r = cpr::Get(cpr::Url{ geturl.str().c_str() }, cpr::VerifySsl(false), cpr::Timeout{ 5000 });
-    r.status_code;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    r.text;                         // JSON text string
-
-    return r.status_code;
-
-}
+#include "..\\GlacialTool_Common\\MiaoSender.cpp"
