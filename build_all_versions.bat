335ffdc1d411cf61e573bb4c37c87c62563369b3
AriaPhantom <32724547+AriaPhantom@users.noreply.github.com>
2026-02-06 13:28:25 -0800

    chore: commit remaining local changes

--- build_all_versions.bat
+++ build_all_versions.bat
@@ -1,38 +1,38 @@
-ï»¿@echo off
+@echo off
 chcp 65001 >nul
-cd /d "D:\OneDrive\Software\Mapleè½¦\GlacialTool_AngelV"
+cd /d "D:\OneDrive\Software\Maple³µ\GlacialTool_AngelV"
 
 set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 echo ========================================
-echo ç¼–è¯‘ Angel å¤šç‰ˆæœ¬ (é€šè¿‡ ProjVer.h)
+echo ±àÒë Angel ¶à°æ±¾ (Í¨¹ý ProjVer.h)
 echo ========================================
 
-:: ç¼–è¯‘ NL ç‰ˆæœ¬
+:: ±àÒë NL °æ±¾
 echo.
-echo [1/3] ç¼–è¯‘ NL ç‰ˆæœ¬...
+echo [1/3] ±àÒë NL °æ±¾...
 echo #pragma once > ProjVer.h
 echo #define NL >> ProjVer.h
-%MSBUILD% Angi.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /m /v:minimal
+%MSBUILD% Angi.sln /t:Build /p:RunPostBuildEvent=Always /p:Configuration=Release /p:Platform=x64 /m /v:minimal
 if exist "x64\Release\Angi.exe" copy /Y "x64\Release\Angi.exe" "x64\Release\Angi_NL.exe"
 
-:: ç¼–è¯‘ Angel ç‰ˆæœ¬
+:: ±àÒë Angel °æ±¾
 echo.
-echo [2/3] ç¼–è¯‘ Angel ç‰ˆæœ¬...
+echo [2/3] ±àÒë Angel °æ±¾...
 echo #pragma once > ProjVer.h
 echo #define Angel >> ProjVer.h
-%MSBUILD% Angi.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /m /v:minimal
+%MSBUILD% Angi.sln /t:Build /p:RunPostBuildEvent=Always /p:Configuration=Release /p:Platform=x64 /m /v:minimal
 if exist "x64\Release\Angi.exe" copy /Y "x64\Release\Angi.exe" "x64\Release\Angi_Angel.exe"
 
-:: ç¼–è¯‘ Adele ç‰ˆæœ¬
+:: ±àÒë Adele °æ±¾
 echo.
-echo [3/3] ç¼–è¯‘ Adele ç‰ˆæœ¬...
+echo [3/3] ±àÒë Adele °æ±¾...
 echo #pragma once > ProjVer.h
 echo #define Adele >> ProjVer.h
-%MSBUILD% Angi.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /m /v:minimal
+%MSBUILD% Angi.sln /t:Build /p:RunPostBuildEvent=Always /p:Configuration=Release /p:Platform=x64 /m /v:minimal
 if exist "x64\Release\Angi.exe" copy /Y "x64\Release\Angi.exe" "x64\Release\Angi_Adele.exe"
 
-:: æ¢å¤é»˜è®¤
+:: »Ö¸´Ä¬ÈÏ
 echo #pragma once > ProjVer.h
 echo #define NL >> ProjVer.h
 
